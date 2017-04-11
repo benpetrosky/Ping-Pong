f321e91b1b139748df84c687ae84108fbdd5b031
Benjamin Petrosky <benpetrosky@hotmail.com>
2017-04-10 21:27:28 -0700

    add functioning scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
-function pingPongList(number) {
+var pingPongList = function (number) {
 
-  var numbers = [];
-  alert(numbers)
+  var output = [];
 
-  // for( i=0; i<numbers.length, i++ ) {
-  //   if (numbers[i] ===)
-  //   numbers.push(numbers[i])
-  // }
-  // numbers.unshift(function(number){
-  //   numbers.push(number * 1)
-  // };
-}
+
+  for(var i=1; i<=number; i++ ) {
+    if (i % 15 === 0)
+    {
+      output.push("ping-pong");
+    }
+    else if (i % 5 === 0)
+    {
+      output.push("pong");
+    }
+    else if (i % 3 === 0)
+    {
+      output.push("ping");
+    }
+    else
+    {
+      output.push(i);
+    }
+    console.log(output)
+  };
+  return output
+};
+
 
 
 
@@ -19,20 +33,17 @@ function pingPongList(number) {
 $(document).ready(function() {
     $("form#number").submit(function(event) {
       event.preventDefault();
-      var finalNumber = $("input#final-number").val();
-      var countingList = pingPongList(finalNumber);
+      var userInput = parseInt($("input#final-number").val());
+      var countingList = pingPongList(userInput);
 
-      $("#countinglist").text(countingList);
+
+
+      $("#counting-list").text(countingList);
 
       $("#counting-list").show();
-    $("#swing").mouseover(function() {
-      $("#swing").attr("src", "img/obama-horz.jpeg");
-    });
-    $("#swing").mouseout(function() {
-      $("#swing").attr("src", "img/obama.jpeg");
     });
   });
-});
+
 
 
 
